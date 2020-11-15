66c2232118757a7bd76d1ee196b9537e271637b7
Alfonsina <alfonsina.l@gmail.com>
2020-11-15 16:59:47 -0300

    First commit. Layout mostly done

---
+++ src/components/Productos.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Productos = () => {
+  return (
+    <>
+      <h2 className="text-center my-5">Listado de Productos</h2>
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Productos
